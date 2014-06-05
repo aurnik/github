fe7fc61403940f8b213ff062c85a2d4567278101
nardecky <nardecky@ucsd.edu>
2014-06-05 02:04:46 -0700

    Events still broke

--- js/events.js
+++ js/events.js
@@ -1,44 +1,16 @@
 $(document).ready(function() {
-      
-      
-	$(".back").hide();
-	$(".hold_image_back1").hide();
-
-
-
-/* just swap images within the same div */
-	$(".front").mouseenter(function() {
-  		$(".back").show();
-  		$(".front").hide();
-	}).mouseleave(function(){
-    	/*$("#img2").hide();    
-    	$("#img1").show();*/
-	});
-
-    $(".back").mouseenter(function() {
-  		/*$("#img1").show();
-  		$("#img2").hide();*/
-	}).mouseleave(function(){ 
-    	$(".front").show();
-    	$(".back").hide();   
-	});
-
-/* hide and show divs */
-$(".hold_image1").mouseenter(function() {
-  		$(".hold_image_back1").show();
-  		$(".hold_image1").hide();
-	}).mouseleave(function(){
-    	/*$("#img2").hide();    
-    	$("#img1").show();*/
-	});
-
-    $(".hold_image_back1").mouseenter(function() {
-  		/*$("#img1").show();
-  		$("#img2").hide();*/
-	}).mouseleave(function(){ 
-    	$(".hold_image1").show();
-    	$(".hold_image_back1").hide();   
-	});
+     
+  //toggle flip'
+  $('.flip-container').click(function()
+  {
+    var child = $(this).children()
+    console.log(child[0])
+    var el = document.querySelector('.card').classList.toggle('flip')  
+    console.log(el)
+    
+  })
+  
+  
 
 
   
